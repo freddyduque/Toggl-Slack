872342af1a155c69ba85bfdb57eda55e42f9e190
Freddy Duque <freddy.duque@gmail.com>
2015-08-08 11:44:02 -0500

    Load from file Feature

--- run_trkr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-tl=60 #tl=3600 #change this to 1 hour = 3600 secs
-while true; do
-	./trkr.rb tknzr --sw="add your webhook URL here" --tt="add your toggl token here" --tl="add the time limit (in seconds) here, every checkpoint or threshold" [--su]="add the slack username or channel to notify here, default: '#general', DM: '@slack.user'" [--bn]="add your personalized Bot Name, default: 'HMD_BOT'" [--db]="add true here if you want to activate debug mode, default: off"   
-	sleep $tl; 
-done
