962b6374a681c549926d022fc9ade5fdfd94305e
Freddy Duque <freddy.duque@gmail.com>
2015-08-13 22:46:54 -0500

    Usage of attr_accessor as setter/getter

--- lib/supervisor.rb
+++ lib/supervisor.rb
@@ -1,67 +1,16 @@
 require 'slack-notifier'
 
 class Supervisor
-	# Initializes the User Class
-	def initialize(webhook,s_channel="#general",s_botname="BOT_HMD",s_link_names=1,s_icon_emoji=":space_invader:")
-		s_channel="#general" if s_channel.nil?
-		s_botname="BOT_HMD" if s_botname.nil?
-		@connection = Slack::Notifier.new webhook, {channel: s_channel, username: s_botname, link_names: s_link_names, icon_emoji: s_icon_emoji}
-		@attachments = Hash.new
-		@main_message = ""
-	end
+  attr_accessor :connection, :message
+    
+  # Initializes the User Class
+  def initialize(webhook)
+    @connection = Slack::Notifier.new webhook, {"channel" => "#general","username"=>"BOT","link_names"=>1}
+    @message = Hash.new
+  end
 
-	# Gets the Channel
-	def get_channel
-		@connection.channel
-	end
-
-	# Gets the Bot Name
-	def get_username
-		@connection.username
-	end
-
-	# Sets the Main and Single Message
-	def set_main_message(main_message)
-		@main_message=main_message
-	end
-
-	# Gets the Main and Single Message
-	def get_main_message()
-		@main_message
-	end
-
-	# Sends the Message
-	def send_message()
-		@connection.ping @main_message, attachments: [@attachments]
-	end
-
-	# Sets the Attachment's Pretext
-	def set_pretext(value)
-		@attachments["pretext"]=value
-	end
-
-	# Sets the Attachment's Text
-	def set_text(value)
-		@attachments["text"]=value
-	end
-
-	# Sets the Attachment's Color
-	def set_color(value)
-		@attachments["color"]=value
-	end
-
-	# Sets the Attachment's Title
-	def set_title(value)
-		@attachments["title"]=value
-	end
-
-	# Sets the Attachment's Link
-	def set_title_link(value)
-		@attachments["title_link"]=value
-	end
-
-	# Gives Format to the Attachment
-	def set_mrkdwn_in(value)
-		@attachments["mrkdwn_in"]=value
-	end
+  # Sends the Message
+  def send(message)
+    @connection.ping message, attachments: [@message]#
+  end
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,62 +1,56 @@
 require_relative 'togglV8'
 
 class User
-	# Initialize the User Class
-	def initialize(token)
-		connection = Toggl.new token
-		@user=connection.me(true)
-	end
+  attr_accessor :information, :time_entry, :notify
 
-	# Get the Current Running Task in Toggl
-	def get_running_task(threshold)		
-		if @user.key?("time_entries")
-			@user["time_entries"].each do |i|
-				if i.key?("stop") == false
-					i.merge!({"z_threshold" => threshold})
-					i.merge!({"z_running_time" => get_running_time(i)})
-					i.merge!(set_notification_type(i))
-					return i
-				end
-			end
-			return {"z_type" => "error", "z_description" => "This User has no Running Tasks", "z_time" => "#{Time.new}"}
-		else
-			return {"z_type" => "error", "z_description" => "This User has no Tasks", "z_time" => "#{Time.new}"}
-		end
-	end
+  def initialize(token,threshold=3600)                 # Initialize the User Class
+    connection = Toggl.new token
+    temp=connection.me(true)    
+    @threshold=threshold
 
-	# Get the User Name
-	def get_user_fullname()
-		@user["fullname"]
-	end	
+    @information = Hash["id" => temp["id"], "email" => temp["email"], "fullname" => temp["fullname"], "z_threshold" => threshold]
+    @information["time_entry"] = @time_entry = running_task(temp["time_entries"])    
+  end
 
-	# Get the task's running time in secs
-	def get_running_time(running_task)
-		current_time = Time.now().localtime("+00:00")
-		running_time = current_time.to_i+running_task["duration"]
-		running_time_verbose = Time.at(running_time).utc.strftime("%H:%M:%S")
-		return running_time.to_i
-	end
+  # Get the Current Running Task in Toggl
+  def running_task(time_entries)
+    unless time_entries.nil?
+      time_entries.each do |i|
+        if i.key?("stop") == false
+          i["z_running_time"] = running_time(i)
+          i["z_notification"] = @notify = notification(i)
+          return i
+        end
+      end
+    end
+    return false
+  end
 
-	# Set the Notification Type
-	def set_notification_type(running_task)
-		threshold_hash = set_notification_threshold(running_task["z_threshold"])
-		case running_task["z_running_time"]
-		when threshold_hash[0][:min] .. threshold_hash[0][:max]
-			return {"z_status" => "Notification","z_color" => "#00CC00"}
-		when threshold_hash[1][:min] .. threshold_hash[1][:max]
-			return {"z_status" => "Warning","z_color" => "#FFFF00"}
-		when threshold_hash[2][:min] .. threshold_hash[2][:max]
-			return {"z_status" => "Danger","z_color" => "#FF8000"}
-		when threshold_hash[3][:min] .. threshold_hash[3][:max]
-			return {"z_status" => "Houston, We've got a Problem!","z_color" => "#FF0000"}
-		else
-			return {"z_type" => "error", "z_description" => "There's no Notification for this User and Task", "z_time" => "#{Time.new}"}
-		end
-	end
+  # Get the task's running time in secs
+  def running_time(task)
+    Time.now().localtime("+00:00").to_i+task["duration"]
+  end
+
+  # Set the Notification Type
+  def notification(task)
+    threshold_hash = notification_threshold(@threshold)
+    case task["z_running_time"]
+    when threshold_hash[0]["min"] .. threshold_hash[0]["max"]
+      return {"z_range" => threshold_hash[0],"z_type" => "Notification","z_color" => "#00CC00"}
+    when threshold_hash[1]["min"] .. threshold_hash[1]["max"]
+      return {"z_range" => threshold_hash[1],"z_type" => "Warning","z_color" => "#FFFF00"}
+    when threshold_hash[2]["min"] .. threshold_hash[2]["max"]
+      return {"z_range" => threshold_hash[2],"z_type" => "Danger","z_color" => "#FF8000"}
+    when threshold_hash[3]["min"] .. threshold_hash[3]["max"]
+      return {"z_range" => threshold_hash[3],"z_type" => "Houston, We've got a Problem!","z_color" => "#FF0000"}
+    else
+      return false                                     # There's no Notification for this User and Task
+    end
+  end
 
 	# Recursive function to set the notifications threshold
-	def set_notification_threshold(threshold,threshold_plus=threshold,i=0,threshold_hash=Hash.new,percent=threshold*0.25)
-		threshold_hash[i] = {min: threshold_plus-percent,max: (threshold_plus+threshold)-percent}
-		i == 3 ? (return threshold_hash) : (set_notification_threshold(threshold,threshold_plus*=2,i+=1,threshold_hash))
-	end
+  def notification_threshold(threshold,threshold_plus=threshold,i=0,threshold_hash=Hash.new,percent=threshold*0.25)
+    threshold_hash[i] = {"min" => threshold_plus-percent,"max" => (threshold_plus+threshold)-percent}
+    i == 3 ? (return threshold_hash) : (notification_threshold(threshold,threshold_plus*=2,i+=1,threshold_hash))
+  end
 end
