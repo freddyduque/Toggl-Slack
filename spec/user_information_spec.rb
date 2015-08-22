e5434562a730a14cad9de8855dcaca6401d2d525
Freddy Duque <freddy.duque@gmail.com>
2015-08-22 10:21:29 -0500

    tested error codes and description, 7 scenarios

--- spec/user_information_spec.rb
+++ spec/user_information_spec.rb
@@ -5,22 +5,21 @@ RSpec.describe User do
     context "with no parameters," do
       it "return the corresponding error and description" do
         user = User.new
-        ap user.information
-        expect(user.information).to eq(error: -1, description: "No Arguments")
+        expect(user.information["z_error"]).to eq([code: -2, description: "No Arguments"])
       end
     end
 
     context "with a wrong Token and just one parameter" do
       it "return the corresponding error and description" do
         user = User.new "1971800d4d82861d8f2c1651fea4d212"
-        expect(user.information).to eq(error: -1, description: "Wrong Token or User doesn't Exist")
+        expect(user.information["z_error"]).to eq([code: -2, description: "User doesn't exist"])
       end
     end
 
     context "with a wrong Token and wrong Time Limit parameter" do
       it "return the corresponding error and description" do
         user = User.new "1971800d4d82861d8f2c1651fea4d212", "123ae"
-        expect(user.information).to eq(error: -1, description: "Wrong Token or User doesn't Exist")
+        expect(user.information["z_error"]).to eq([{:code=>-1, :description=>"Time Limit is not Integer"}, {:code=>-2, :description=>"User doesn't exist"}])
       end
     end
 
@@ -34,23 +33,22 @@ RSpec.describe User do
     context "with an existing Token and wrong Time Limit parameter" do
       it "return the corresponding error and description" do
         user = User.new "d396742070d0b5e3ae602c7baca8bb3d", "aed222"
-        expect(user.information).to eq(error: -1, description: "Time Limit is not Numeric or Higher than 1 second")
+        expect(user.information["z_error"]).to eq([code: -1, description: "Time Limit is not Integer"])
       end
     end
 
-    context "with an existing Token and wrong Time Limit parameter" do
+    context "with an existing Token and lower than 1 Time Limit parameter" do
       it "return the corresponding error and description" do
-        user = User.new "d396742070d0b5e3ae602c7baca8bb3d", 1
-        expect(user.information).to eq(error: -1, description: "Time Limit is not Numeric or Higher than 1 second")
+        user = User.new "d396742070d0b5e3ae602c7baca8bb3d", 0
+        expect(user.information["z_error"]).to eq([code: -1, description: "Time Limit is lower than 1 second"])
       end
     end
-=begin
-    context "with a wrong Token" do
-      it "return 'nil' if the user doesn't exit" do
-        user = User.new "1971800d4d82861d8f2c1651fea4d212"
-        expect(user.information).to eq(nil)
+
+    context "with an existing Token and lower than 1 Time Limit parameter" do
+      it "return the corresponding error and description" do
+        user = User.new "d396742070d0b5e3ae602c7baca8bb3d", 0.9
+        expect(user.information["z_error"]).to eq([code: -1, description: "Time Limit is lower than 1 second"])
       end
     end
-=end
   end
 end
