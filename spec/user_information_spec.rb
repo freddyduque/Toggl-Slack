0ee8210c77b024942ab78c08fd57948359da0029
Freddy Duque <freddy.duque@gmail.com>
2015-09-03 14:47:50 -0500

    filtered testing cases

--- spec/user_information_spec.rb
+++ spec/user_information_spec.rb
@@ -23,31 +23,14 @@ RSpec.describe User do
     context "When parameters are Wrong" do
       it "returns a Parameters error" do
         expect((User.new token[:wrong]).information).to have_key("z_error")           # code: -2, description: "User doesn't exist"
-        expect((User.new token[:wrong],"10").information).to have_key("z_error")      # code: -2, description: "User doesn't exist"
-        expect((User.new token[:wrong],"1.6").information).to have_key("z_error")     # code: -2, description: "User doesn't exist"
-        expect((User.new token[:wrong], 60).information).to have_key("z_error")       # code: -2, description: "User doesn't exist"
-        expect((User.new token[:wrong], 10.5).information).to have_key("z_error")     # code: -2, description: "User doesn't exist"
-
-        expect((User.new token[:wrong], "").information).to have_key("z_error")       # code: -1, description: "Time Limit is not Integer";  code: -2, description: "User doesn't exist"
         expect((User.new token[:wrong], "32s2").information).to have_key("z_error")   # code: -1, description: "Time Limit is not Integer";  code: -2, description: "User doesn't exist"
-
-        expect((User.new token[:wrong], "-3").information).to have_key("z_error")     # code: -1, description: "Time Limit is lower than 1 second";  code: -2, description: "User doesn't exist"
         expect((User.new token[:wrong], -3).information).to have_key("z_error")       # code: -1, description: "Time Limit is lower than 1 second";  code: -2, description: "User doesn't exist"
 
         expect((User.new "").information).to have_key("z_error")                      # code: -2, description: "User Token is empty"
-        expect((User.new "","10").information).to have_key("z_error")                 # code: -2, description: "User Token is empty"
-        expect((User.new "","1.6").information).to have_key("z_error")                # code: -2, description: "User Token is empty"
-        expect((User.new "", 60).information).to have_key("z_error")                  # code: -2, description: "User Token is empty"
-        expect((User.new "", 10.5).information).to have_key("z_error")                # code: -2, description: "User Token is empty"
-        expect((User.new "","").information).to have_key("z_error")                   # code: -1, description: "Time Limit is not Integer";  code: -2, description: "User Token is empty"
         expect((User.new "","32s2").information).to have_key("z_error")               # code: -1, description: "Time Limit is not Integer";  code: -2, description: "User Token is empty"
-        expect((User.new "","-3").information).to have_key("z_error")                 # code: -1, description: "Time Limit is lower than 1 second";  code: -2, description: "User Token is empty"
         expect((User.new "",-3).information).to have_key("z_error")                   # code: -1, description: "Time Limit is lower than 1 second";  code: -2, description: "User Token is empty"
-        expect((User.new "","10").information).to have_key("z_error")                 # code: -1, description: "Time Limit is lower than 1 second";  code: -2, description: "User Token is empty"
 
-        expect((User.new token[:right], "").information).to have_key("z_error")       # code: -1, description: "Time Limit is not Integer"
         expect((User.new token[:right], "32s2").information).to have_key("z_error")   # code: -1, description: "Time Limit is not Integer"
-        expect((User.new token[:right], "-3").information).to have_key("z_error")     # code: -1, description: "Time Limit is lower than 1 second"
         expect((User.new token[:right], -3).information).to have_key("z_error")       # code: -1, description: "Time Limit is lower than 1 second"
       end
     end
